c1545307859c515b6edb6fdef3342c178dcdbac3
Jason Barto <jason.p.barto@gmail.com>
2019-08-25 00:21:42 +0100

    cleared private values from variables.sh

--- variables.sh
+++ variables.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-CLUSTER_NAME='fra-eks-6'
+CLUSTER_NAME='private-eks-cluster'
 REGION=eu-central-1
-HTTP_PROXY_ENDPOINT_SERVICE_NAME=com.amazonaws.vpce.eu-central-1.vpce-svc-036915ed05f9700df
-KEY_PAIR=jasbarto-dev-fra
+HTTP_PROXY_ENDPOINT_SERVICE_NAME=com.amazonaws.vpce.eu-central-1.vpce-svc-012345ed05f9700df
+KEY_PAIR=MY-EC2-KEYPAIR
 VERSION='1.13'
 AMI_ID=ami-038bd8d3a2345061f
 INSTANCE_TYPE=t3.medium
-S3_STAGING_LOCATION=jasbarto-dev-frankfurt-cloudformation
+S3_STAGING_LOCATION=MY-BUCKET/MY/PREFIX
